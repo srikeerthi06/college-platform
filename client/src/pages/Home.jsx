46c3b937c6e0e364db893773ccd645fd0e7776ce
srikeerthi06 <srikeerthireddy14@gmail.com>
2026-05-06 15:06:12 +0530

    removed localhost api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../services/api";
+import API from "../services/api.js";
 
 function Home() {
   const [colleges, setColleges] = useState([]);
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({
+export default axios.create({
   baseURL: "https://college-backend-q4n4.onrender.com",
 });
-
-export default API;
